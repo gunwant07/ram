6f17adfa60871c61a5930f46aa30d2ff9ebf42f1
gunwant07 <144123267+gunwant07@users.noreply.github.com>
2023-10-03 19:29:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const express=require('express')
-const server =express();
-const bodyparser=require('body-parser')
-const port=parseInt(process.env.PORT) || 3000;
-server.use(bodyparser.json());
-const mongoose=require('mongoose');
-const path=require("path");
-require("dotenv").config({
-  path: path.join(__dirname,"config.env")
-})
-main().catch(err => console.log(err));
+const express = require('express');
+const server = express();
+const port = process.env.PORT || '3000';
+
+server.use(express.json());
+
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, 'config.env'),
+});
+
+main().catch((err) => console.log(err));
+
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log("connected");
+  console.log('connected');
 }
-const userSchema = new mongoose.Schema({
-    username:String,
-    password:String
-  });
-  const User = mongoose.model('user', userSchema);
-server.post('/demo',async(req,res)=>{
-    let user=new User();
-    user.username=req.body.username;
-    user.password=req.body.password;
-    const doc= await user.save();
-   console.log(doc);
-    res.json( doc);
 
-})
-server.listen(port,()=>{
-    console.log('server started');
-    module.exports=server;
-})
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('user', userSchema);
+
+server.post('/demo', async (req, res) => {
+  let user = new User();
+  user.username = req.body.username;
+  user.password = req.body.password;
+  const doc = await user.save();
+  console.log(doc);
+  res.json(doc);
+});
+
+server.listen(port, () => {
+  console.log('server started');
+});
